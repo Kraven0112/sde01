8b8619cf0dd1c713a78e9b31b85b303aae790f9a
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-03-25 14:40:57 +0530

    new one

---
+++ Basic/script.js
@@ -0,0 +1,7 @@
+const person = { name: "Bob", age: 30 };
+const jsonString = JSON.stringify(person);
+console.log(jsonString); // '{"name":"Bob","age":30}'
+
+// JSON.stringify() is used to convert json object into json string
+// JSON.parse() is used to convert json stringify into json object
+
