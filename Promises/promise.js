b61ddd4fbf1944546b013656c5f7da2ce9655641
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-04-12 21:39:21 +0545

    add new one

--- Promises/promise.js
+++ Promises/promise.js
@@ -3,7 +3,7 @@
     There are different types of promise methods:
     Promise.all() => takes array as an arguments and gives all resolved response if all promises are resolved but gives rejected response if one the promises is rejected.
 
-    Promise.any() => takes array as an arguments and seeks for resolve promise, if it gets promise resolved it responses that promise is resolved. If all promises are rejected it gives aggregated error (means array of rejection and error).
+    Promise.any() => takes array as an arguments and seeks for resolved promise, if it gets promise resolved it responses that promise is resolved. If all promises are rejected it gives aggregated error (means array of rejection and error).
 
     Promise.race() => takes array as an arguments and gives response either promise is resolved or rejected. It depends on time that how much time promise takes to get resolved or rejected.
 
@@ -26,27 +26,28 @@ const promise2 = new Promise(function(resolve,reject){
 
 const promise3 = new Promise(function(resolve,reject){
     setTimeout(() => {
-        resolve("promise3 is resolved")
-        // reject("promise3 is rejected")
+        // resolve("promise3 is resolved")
+        reject("promise3 is rejected")
     }, 5000);
 })
 
 // Promise.all([promise1,promise2,promise3])
 //     .then((data) => {
 //         const[res1,res2,res3] = data
-//         console.log(res1)
-//         console.log(res2)
-//         console.log(res3)
+//         // console.log(res1)
+//         // console.log(res2)
+//         // console.log(res3)
+//         console.log(data);
 //     })
 //     .catch(error => console.error(error))
 
 
 
-// Promise.any([promise1,promise2,promise3])
-//     .then((data) => {
-//         console.log(data)
-//     })
-//     .catch(error => console.error(error))
+Promise.any([promise1,promise2,promise3])
+    .then((data) => {
+        console.log(data)
+    })
+    .catch(error => console.error(error))
 
 
 // Promise.race([promise1,promise2,promise3])
@@ -56,9 +57,9 @@ const promise3 = new Promise(function(resolve,reject){
 //     .catch(error => console.error(error))
 
     
-Promise.allSettled([promise1,promise2,promise3])
-    .then((data) => {
-        console.log(data)
-    })
-    .catch(error => console.error(error))
+// Promise.allSettled([promise1,promise2,promise3])
+//     .then((data) => {
+//         console.log(data)
+//     })
+//     .catch(error => console.error(error))
 
