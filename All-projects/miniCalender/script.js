b5f74e97967a1e5422c4e362977d9dc1863fa1f5
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-04-22 22:04:50 +0545

    add new feature

--- All-projects/miniCalender/script.js
+++ All-projects/miniCalender/script.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function(){
     const monthDisplay = document.getElementById('month')
     const dateDisplay = document.getElementById('date')
     const dayDisplay = document.getElementById('day')
+    const yearDisplay = document.getElementById('year')
     
     const date = new Date()
 
     monthDisplay.textContent = date.toLocaleDateString('default', { month: 'long' })
     dateDisplay.textContent = date.getDate()
     dayDisplay.textContent = date.toLocaleDateString('en-US', { weekday: 'long' })
+    yearDisplay.textContent = date.getFullYear()
 
     document.getElementById('mainDiv').addEventListener('click', function(){
         document.getElementById('mainDiv').classList.toggle('bgc')
