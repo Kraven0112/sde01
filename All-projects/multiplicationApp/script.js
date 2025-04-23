299d99306d3a5309b06bf26651a72501e52cef84
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-04-23 15:29:54 +0545

    add new component

--- All-projects/multiplicationApp/script.js
+++ All-projects/multiplicationApp/script.js
@@ -1,3 +1,48 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const scoreDisplay = document.getElementById('score')
+    const multiplicandDisplay = document.getElementById('multiplicand')
+    const multiplierDisplay = document.getElementById('multiplier')
+    const answerInput = document.getElementById('answer')
+    const form = document.getElementById('inputForm')
+    const wrongMessage = document.getElementById('wrongMessage')
+
+    let score = 0
+    let multiplicand = generateMultiliplicand()
+    let multiplier = generateMultiplier()
+
+    displayAllitem()
     
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        
+        if(!answerInput) return;
+
+        const answerValue = parseInt(answerInput.value.trim())
+
+        if (answerValue === (multiplicand * multiplier) && answerValue !== ""){
+            score  = score + 1
+            scoreDisplay.textContent = `Score : ${score}`
+            wrongMessage.classList.add('hidden')
+            multiplicand = generateMultiliplicand()
+            multiplier = generateMultiplier()
+            displayAllitem()
+        }else{
+            wrongMessage.classList.remove('hidden')
+        }
+
+        answerInput.value = ""
+
+    })
+
+    function generateMultiliplicand(){
+        return Math.floor(Math.random() * 10)
+    }
+    function generateMultiplier(){
+        return Math.floor(Math.random() * 10)
+    }
+
+    function displayAllitem(){
+        multiplicandDisplay.textContent = multiplicand
+        multiplierDisplay.textContent = multiplier
+    }
 })
