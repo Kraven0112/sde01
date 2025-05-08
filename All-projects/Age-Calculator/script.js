f94ab6c029a719316e3846068eb1239d26e2a601
Mike <sushilchaudhary2059@gmail.com>
2025-05-08 23:03:48 +0545

    add new project age calculator

---
+++ All-projects/Age-Calculator/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const birthdayInput = document.getElementById('birthday')
+    const form = document.getElementById('inputField')
+    const result = document.getElementById('result')
+    const message = document.getElementById('message')
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        getAge()
+    })
+
+    function getAge() {
+        const birthday = birthdayInput.value
+
+        if (birthday === "") {
+            message.classList.remove('hidden')
+        } else {
+            message.classList.add('hidden')
+            const age = convertAge(birthday)
+            result.innerHTML = `Your age is ${age} ${age > 1 ? "years" : "year"} old.`
+        }
+
+        birthdayInput.value = ""
+    }
+
+    function convertAge(birthday) {
+        const currentDate = new Date()
+        const birthdayDate = new Date(birthday)
+        let age = currentDate.getFullYear() - birthdayDate.getFullYear()
+
+        let month = currentDate.getMonth() - birthdayDate.getMonth()
+
+        if (month < 0 || (month === 0 && currentDate.getDate() > birthdayDate.getDate())) {
+            age--
+        }
+
+        return age
+    }
+})
